6ddb9bfa7206efa071a5a4d73371a2bfcd9d9344
moda-it <demianchykbogdan@gmail.com>
2025-11-11 21:08:05 +0200

    Add project files.

---
+++ IsertSort/IsertSort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	const int N = 10;
+	int a[N];
+	cout << "Введіть " << N << " чисел:\n";
+	for (int i = 0; i < N; i++) {
+		cout << "a[" << i << "] = ";
+		cin >> a[i];
+	}
+
+	for (int i = 1; i < N; i++) {
+		int key = a[i];
+		int j = i - 1;
+
+		while (j >= 0 && a[j] > key) {
+			a[j + 1] = a[j];
+			j--;
+		}
+		a[j + 1] = key;
+	}
+
+	cout << "\nВідсортований масив:\n";
+	for (int i = 0; i < N; i++) {
+		cout << a[i] << '\t';
+	}
+	cout << endl;
+
+	return 0;
+}
